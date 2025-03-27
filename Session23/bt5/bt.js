ec416d3449ed56f79a594829b8114a831bfc82bd
tnhan2905 <diepthanhnhan2019.st@gmail.com>
2025-03-27 08:21:33 +0700

    Update bt.js

--- Session23/bt5/bt.js
+++ Session23/bt5/bt.js
@@ -1,24 +1,25 @@
-let arr = []
-let n = +prompt('nhập vào số phần tử của mảng')
-for (let i = 1; i <= n; i++){
-    arr[i] = Number(prompt(`nhập vào phân tử index thứ ${i}`))
+let arr = [];
+let n = +prompt('Nhập vào số phần tử của mảng');
+if (n < 0) {
+    console.log("Số lượng phần tử không được âm");
+} else if (n === 0) {
+    console.log("Mảng không có phần tử");
+} else {
+for (let i = 0; i < n; i++) {
+    arr[i] = Number(prompt(`Nhập vào phần tử index thứ ${i}`));
 }
-let count = 0, sum = 0
-for (let i = 1; i <= n; i++){
-    if (isNaN(Number(arr[i]))){
-        continue
-    }else{
-        sum += Number(arr[i])
-        count ++
+let count = 0, sum = 0;
+for (let i = 0; i < n; i++) {
+if (isNaN(arr[i])) {
+    continue;
+    } else {
+        sum += arr[i];
+        count++;
     }
 }
-if(count === 0 && n > 0){
-    console.log(`Không có phần tử nào là số`)
-}
-if(n === 0){
-    console.log(`Mảng không có phần tử`)
-}else if(n < 0){
-    console.log(`Số lượng phần tử không được âm`)
-}else if (count > 0) {
-    console.log(sum)
+if (count === 0) {
+  console.log(`Không có phần tử nào là số`);
+} else {
+  console.log(`Tổng các số hợp lệ trong mảng: ${sum}`);
+  }
 }

0d7a13d947364da84cc9807e07e71a83c1d63541
tnhan2905 <diepthanhnhan2019.st@gmail.com>
2025-03-27 08:20:25 +0700

    Update bt.js

--- Session23/bt3/bt.js
+++ Session23/bt3/bt.js
@@ -1,18 +1,16 @@
 let arr = []
 let n = +prompt('nhập vào số phần tử của mảng')
-for (let i = 1; i <= n; i++){
-    arr[i] = Number(prompt(`nhập vào phân tử index thứ ${i}`))
+for (let i = 0; i < n; i++){
+    arr[i] = Number(prompt(`Nhập vào phần tử index thứ ${i}`));
 }
 let count = 0
-for (let i = 1; i <= n; i++){
+for (let i = 0; i < n; i++){
     if (arr[i] < 0 && Number.isInteger(arr[i])){
-        count ++
+        count++;
     }
 }
-if(n === 0){
-    console.log(`Mảng không có phần tử`)
-}else if(n < 0){
-    console.log(`Số lượng phần tử không hợp lệ`)
-}else{
-    console.log(count)
+if (n < 0) {
+    console.log("Số lượng phần tử không hợp lệ");
+} else if (n === 0) {
+    console.log("Mảng không có phần tử");
 }
